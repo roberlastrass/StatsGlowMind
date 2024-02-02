08c5b9ffc561f1b83aa7c3df6fa8550cf966cb47
roberlastrass <roberlastrass14@gmail.com>
2024-02-02 11:07:02 +0100

    error de compilacion solucionado

--- stats-glow-mind/src/app/app.config.ts
+++ stats-glow-mind/src/app/app.config.ts
@@ -15,9 +15,27 @@ import { getStorage, provideStorage } from '@angular/fire/storage';
 import { getRemoteConfig, provideRemoteConfig } from '@angular/fire/remote-config';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"statsglowmindtfg","appId":"1:111604075463:web:c135dffece96e3791c67e2","databaseURL":"https://statsglowmindtfg-default-rtdb.europe-west1.firebasedatabase.app","storageBucket":"statsglowmindtfg.appspot.com","apiKey":"AIzaSyA-n33foAxVcuWuvKWVIuaAan2Q9YMShl4","authDomain":"statsglowmindtfg.firebaseapp.com","messagingSenderId":"111604075463","measurementId":"G-RYGMD1ENK6"}))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideAnalytics(() => getAnalytics())), ScreenTrackingService, UserTrackingService, importProvidersFrom(provideAppCheck(() => {
-  // TODO get a reCAPTCHA Enterprise here https://console.cloud.google.com/security/recaptcha?project=_
-  const provider = new ReCaptchaEnterpriseProvider(/* reCAPTCHA Enterprise site key */);
-  return initializeAppCheck(undefined, { provider, isTokenAutoRefreshEnabled: true });
-})), importProvidersFrom(provideFirestore(() => getFirestore())), importProvidersFrom(provideDatabase(() => getDatabase())), importProvidersFrom(provideFunctions(() => getFunctions())), importProvidersFrom(provideMessaging(() => getMessaging())), importProvidersFrom(providePerformance(() => getPerformance())), importProvidersFrom(provideStorage(() => getStorage())), importProvidersFrom(provideRemoteConfig(() => getRemoteConfig()))]
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(provideFirebaseApp(() => initializeApp({
+      "projectId": "statsglowmindtfg",
+      // ... otras configuraciones de Firebase ...
+    }))),
+    importProvidersFrom(provideAuth(() => getAuth())),
+    importProvidersFrom(provideAnalytics(() => getAnalytics())),
+    ScreenTrackingService,
+    UserTrackingService,
+    importProvidersFrom(provideAppCheck(() => {
+      // TODO: Reemplaza 'reCAPTCHA_Enterprise_Site_Key' con tu clave del sitio reCAPTCHA Enterprise
+      const provider = new ReCaptchaEnterpriseProvider('reCAPTCHA_Enterprise_Site_Key');
+      return initializeAppCheck(undefined, { provider, isTokenAutoRefreshEnabled: true });
+    })),
+    importProvidersFrom(provideFirestore(() => getFirestore())),
+    importProvidersFrom(provideDatabase(() => getDatabase())),
+    importProvidersFrom(provideFunctions(() => getFunctions())),
+    importProvidersFrom(provideMessaging(() => getMessaging())),
+    importProvidersFrom(providePerformance(() => getPerformance())),
+    importProvidersFrom(provideStorage(() => getStorage())),
+    importProvidersFrom(provideRemoteConfig(() => getRemoteConfig()))
+  ]
 };
