cb806f4f1231065cbc279ae3bc04db9e392f3411
roberlastrass <roberlastrass14@gmail.com>
2024-02-14 15:34:24 +0100

    registro, login y logout de un usuario

--- stats-glow-mind/src/app/app-routing.module.ts
+++ stats-glow-mind/src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
+import { MainComponent } from './components/main/main.component';
 
 const routes: Routes = [
-  { path: '', component: AppComponent },
+  { path: '', component: MainComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'main', component: MainComponent }
 ];
 
 @NgModule({
